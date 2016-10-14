9f15ca00dbf646abcee58fc4013e0bf48d0ad6ca
abeers <alexbeers92@gmail.com>
2016-10-14 13:06:54 -0400

    Refactors into an upload module
    Extracts AWS S3 and methods related to file upload
    Leaves code pertaining to CLI inside bin/s3-upload.js
    
    This module is usable from both the command line application and an
    express API server

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,101 +1,30 @@
 'use strict';
 
-require('dotenv').config();
-
 const fs = require('fs');
-const crypto = require('crypto');
-const fileType = require('file-type');
-const AWS = require('aws-sdk');
+
+const upload = require('../lib/s3-upload').upload;
 
 const filename = process.argv[2] || '';
 
 const readFile = (filename) => {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fs.readFile(filename, (error, data) => {
       if (error) {
         reject(error);
       }
-      resolve(data);
-    });
-  });
-};
-
-const mimeType = (data) => {
-  return Object.assign({
-    ext: 'bin',
-    mime: 'application/octet-stream'
-  }, fileType(data));
-};
-
-const parseFile = (fileBuffer) => {
-  let file = mimeType(fileBuffer);
-  file.data = fileBuffer;
-  return file;
-};
-
-const randomHexString = (length) => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(length, (error, buffer) => {
-      if (error) {
-        reject(error);
-      }
-
-      resolve(buffer.toString('hex'));
-    });
-  });
-};
-
-const nameFile = (file) => {
-  return randomHexString(16)
-  .then((val) => {
-    file.name = val;
-    return file;
-  });
-};
-
-const nameDirectory = (file) => {
-  file.dir = new Date().toISOString().split('T')[0];
-  return file;
-};
-
-const s3 = new AWS.S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
-});
-
-const upload = (file) => {
-  const options = {
-    Bucket: 'bucketforbeers',
-    Body: file.data,
-    ACL: 'public-read',
-    ContentType: file.mime,
-    Key: `${file.dir}/${file.name}.${file.ext}`
-  };
-
-  return new Promise((resolve, reject) => {
-    s3.upload(options, (error, data) => {
-      if (error) {
-        reject(error);
-      }
 
       resolve(data);
     });
   });
 };
 
-const logMessage = (upload) => {
-  delete upload.Body;
-  console.log(`The upload from AWS was ${upload}`);
+const logMessage = (response) => {
+  // turn the pojo into a string so I can see it on the console
+  console.log(`the response from AWS was ${JSON.stringify(response)}`);
 };
 
 readFile(filename)
-.then(parseFile)
-.then(nameFile)
-.then(nameDirectory)
 .then(upload)
-// randomHexString(6)
-// .then(console.log)
 .then(logMessage)
-.catch(console.error);
+.catch(console.error)
+;
