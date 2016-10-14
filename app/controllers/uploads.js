23464f9f1a0e57a82f55775db97afc708a1da999
abeers <alexbeers92@gmail.com>
2016-10-14 15:23:22 -0400

    Completes file upload on server
    Uses upload library to initiate upload to AWS
    Store response from AWS in Mongo
    Send JSON back to client

---
+++ app/controllers/uploads.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const controller = require('lib/wiring/controller');
+const multer = require('app/middleware').multer;
+
+const models = require('app/models');
+const Upload = models.upload;
+
+const upload = require('lib/s3-upload').upload;
+
+// const authenticate = require('./concerns/authenticate');
+
+const index = (req, res, next) => {
+  Upload.find()
+    .then(uploads => res.json({ uploads }))
+    .catch(err => next(err));
+};
+
+const show = (req, res, next) => {
+  Upload.findById(req.params.id)
+    .then(upload => upload ? res.json({ upload }) : next())
+    .catch(err => next(err));
+};
+
+const create = (req, res, next) => {
+  // let upload = Object.assign(req.body.upload, {
+  //   _owner: req.currentUser._id,
+  // });
+
+  upload(req.file.buffer)
+  .then((response) => {
+    return {
+      location: response.Location,
+      comment: req.body.upload.comment
+    };
+  })
+  .then(upload => Upload.create(upload))
+  .then(upload => res.json({ upload }))
+  .catch(error => next(error))
+  ;
+};
+
+// const update = (req, res, next) => {
+//   let search = { _id: req.params.id, _owner: req.currentUser._id };
+//   Upload.findOne(search)
+//     .then(upload => {
+//       if (!upload) {
+//         return next();
+//       }
+//
+//       delete req.body._owner;  // disallow owner reassignment.
+//       return upload.update(req.body.upload)
+//         .then(() => res.sendStatus(200));
+//     })
+//     .catch(err => next(err));
+// };
+//
+// const destroy = (req, res, next) => {
+//   let search = { _id: req.params.id, _owner: req.currentUser._id };
+//   Upload.findOne(search)
+//     .then(upload => {
+//       if (!upload) {
+//         return next();
+//       }
+//
+//       return upload.remove()
+//         .then(() => res.sendStatus(200));
+//     })
+//     .catch(err => next(err));
+// };
+
+module.exports = controller({
+  index,
+  show,
+  create,
+  // update,
+  // destroy,
+}, { before: [
+  // { method: authenticate, except: ['index', 'show'] },
+  { method: multer.single('upload[file]'), only: ['create']}
+], });
