d5a41322381c637743c7a92367f1005a4575a914
abeers <alexbeers92@gmail.com>
2016-10-14 11:45:58 -0400

    Uploads to AWS with random filename
    Brings in crypto
    Generates random filename based on 16 byte hex
    Generates directory name based on date
    Uploads to my bucket

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,7 +1,11 @@
 'use strict';
 
+require('dotenv').config();
+
 const fs = require('fs');
+const crypto = require('crypto');
 const fileType = require('file-type');
+const AWS = require('aws-sdk');
 
 const filename = process.argv[2] || '';
 
@@ -29,24 +33,69 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
+const randomHexString = (length) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (error, buffer) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(buffer.toString('hex'));
+    });
+  });
+};
+
+const nameFile = (file) => {
+  return randomHexString(16)
+  .then((val) => {
+    file.name = val;
+    return file;
+  });
+};
+
+const nameDirectory = (file) => {
+  file.dir = new Date().toISOString().split('T')[0];
+  return file;
+};
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
+
 const upload = (file) => {
   const options = {
     Bucket: 'bucketforbeers',
     Body: file.data,
     ACL: 'public-read',
     ContentType: file.mime,
-    Key: `test/test.${file.ext}`
+    Key: `${file.dir}/${file.name}.${file.ext}`
   };
-  return Promise.resolve(options);
+
+  return new Promise((resolve, reject) => {
+    s3.upload(options, (error, data) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(data);
+    });
+  });
 };
 
 const logMessage = (upload) => {
   delete upload.Body;
-  console.log(`The upload options are ${JSON.stringify(upload)}`);
+  console.log(`The upload from AWS was ${upload}`);
 };
 
 readFile(filename)
 .then(parseFile)
+.then(nameFile)
+.then(nameDirectory)
 .then(upload)
+// randomHexString(6)
+// .then(console.log)
 .then(logMessage)
 .catch(console.error);
